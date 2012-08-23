9f6058851d7cfa2362c9acaeccb29466890aace6
Peter Bergström <petb@redpill-linpro.com>
2012-08-23 09:25:13 +0200

    Wrong Smarty class

---
+++ classes/MySmarty.php
@@ -0,0 +1,13 @@
+<?php
+require dirname(__FILE__) . '/smarty/Smarty.class.php';
+class MySmarty extends Smarty
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplateDir('Templates');
+        $this->setCacheDir('Cache/Smarty/Cache');
+        $this->setCompileDir('Cache/Smarty/Compiled');
+        $this->assign('baseUrl', Homelink::BASE_URL);
+    }
+}
