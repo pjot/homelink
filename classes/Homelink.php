34f77927d5e6b28e9848137b53a499495dbafd19
Peter Bergström <petb@redpill-linpro.com>
2012-08-24 08:07:50 +0200

    Removed const's and introduced a config file

--- classes/Homelink.php
+++ classes/Homelink.php
@@ -1,12 +1,21 @@
 <?php
 class Homelink
 {
-	const BASE_URL = 'http://pjot.se/homelink';
-	const TORRENT_PATH = '/home/pjot/Downloads/';
-	const SEED_PATH = '/media/store/tmp/';
-	const VIEW_PATH = '/media/store/share/';
-
 	private $action = null;
+    private static $config = null;
+
+    public static function getConfig($config)
+    {
+        return self::$config[$config];
+    }
+
+    private static function loadConfig()
+    {
+        if ( ! isset(self::$config))
+        {
+            self::$config = parse_ini_file(dirname(___FILE___));
+        }
+    }
 
 	private static function redirect($action)
 	{
@@ -36,25 +45,25 @@ class Homelink
 			'seed' => array(
 				'name' => 'Seeding',
 				'active' => $this->action == 'seed',
-				'url' =>  self::BASE_URL . '?action=seed',
+				'url' =>  self::getConfig('BASE_URL') . '?action=seed',
 				'icon' => 'icon-hdd',
 			),
 			'view' => array(
 				'name' => 'Viewing',
 				'active' => $this->action == 'view',
-				'url' => self::BASE_URL . '?action=view',
+				'url' => self::getConfig('BASE_URL') . '?action=view',
 				'icon' => 'icon-film',
 			),
 			'deluge' => array(
 				'name' => 'Downloading',
 				'active' => $this->action == 'deluge',
-				'url' => self::BASE_URL . '?action=deluge',
+				'url' => self::getConfig('BASE_URL') . '?action=deluge',
 				'icon' => 'icon-tasks',
 			),
 			'torrent' => array(
 				'name' => 'Add Torrent',
 				'active' => $this->action == 'torrent',
-				'url' => self::BASE_URL . '?action=torrent',
+				'url' => self::getconfig('BASE_URL') . '?action=torrent',
 				'icon' => 'icon-download-alt',
 			)
 		    )
@@ -85,7 +94,7 @@ class Homelink
 
 	private function actionSeed()
 	{
-		$folder = self::SEED_PATH . (isset($_GET['folder']) ? preg_replace('/\.\.\/?/', '', $_GET['folder']) : '');
+		$folder = self::getConfig('SEED_PATH') . (isset($_GET['folder']) ? preg_replace('/\.\.\/?/', '', $_GET['folder']) : '');
 		$directory = new DirectoryLister($folder);
 		$entries = array();
 		foreach ($directory->getFiles() as $file)
@@ -104,11 +113,11 @@ class Homelink
 
 	private function actionView()
 	{
-		$directory = new DirectoryLister(self::VIEW_PATH);
+		$directory = new DirectoryLister(self::getConfig'(VIEW_PATH'));
 		$entries = array();
 		foreach ($directory->getFiles() as $file)
 		{
-		    $entries[] = new ViewEntry(self::VIEW_PATH . $file);
+		    $entries[] = new ViewEntry(self::getConfig('VIEW_PATH') . $file);
 		}
 		usort($entries, array('Entry', 'sort'));
 		$view = new FileView();
@@ -131,7 +140,7 @@ class Homelink
 	private function actionToggle()
 	{
 		$target = preg_replace('/\.\.\/?/', '', $_GET['target']);
-		if (strpos($target, self::SEED_PATH) !== false)
+		if (strpos($target, self::getConfig('SEED_PATH')) !== false)
 		{
 			if (Toggler::makeLink($target))
 			{
@@ -148,7 +157,7 @@ class Homelink
 				);
 			}
 		}
-		if (strpos($target, self::VIEW_PATH) !== false)
+		if (strpos($target, self::getConfig('VIEW_PATH')) !== false)
 		{
 			if (Toggler::removeLink($target))
 			{
